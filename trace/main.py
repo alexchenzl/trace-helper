dca0ed7f722b7a9e56b92ff0936175361a79c720
achen <greedyalex@gmail.com>
2022-02-28 16:32:26 +0800

    optimize access list tracer, save result into json files and add command arguments

--- trace/main.py
+++ trace/main.py
@@ -3,46 +3,158 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
-import sys
 import json
-from web3 import HTTPProvider
+import argparse
+import datetime
 
+from web3 import HTTPProvider, Web3
 from trace.tracer.tracer import load_tracer
 
 
-def trace_tx(provider: HTTPProvider, tx_hash, tracer):
+def trace_tx(provider: HTTPProvider, tx_hash, tracer, timeout=10):
     params = [tx_hash, {
-        'disableStack': False,
-        'disableMemory': False,
-        'disableStorage': True}]
+        'disableStack': True,
+        'disableMemory': True,
+        'disableStorage': True,
+        'timeout': str(timeout) + 's',
+    }]
     if tracer is not None:
         params[1]['tracer'] = tracer
     return provider.make_request('debug_traceTransaction', params)
 
 
-def print_help():
-    help_str = '''
-        Usage: pipenv run main.py provider_url tx_hash [tracer]
-        tracer is optional, its value could be ac_tracer or dis_tracer, 
-        '''
-    print(help_str)
+def trace_block(provider: HTTPProvider, block_num, tracer, timeout=5):
+    params = [block_num, {
+        'disableStack': True,
+        'disableMemory': True,
+        'disableStorage': True,
+        'timeout': str(timeout) + 's',
+    }]
+    if tracer is not None:
+        params[1]['tracer'] = tracer
+    return provider.make_request('debug_traceBlockByNumber', params)
 
+
+def fetch_block(provider: HTTPProvider, block_num):
+    w3 = Web3(provider)
+    return w3.eth.get_block(block_num)
+
+
+def print_stack(stack, tabs):
+    print("{}Stack:".format(tabs))
+    for item in stack:
+        print("\t{0}0x{1}".format(tabs, item))
+
+
+def print_opcodes(opcodes, tabs=""):
+    for opcode in opcodes:
+        op = opcode['op']
+        if op == 'CALL' or op == 'CALLCODE' or op == 'DELEGATECALL' or op == 'STATICCALL':
+            line = "\n{0}Step={1:0>8x} {2:<16}\tpc={3:0>8x} depth={4} return={5}".format(tabs, opcode['step'],
+                                                                                         opcode['op'],
+                                                                                         opcode['pc'],
+                                                                                         opcode['depth'],
+                                                                                         opcode['return'])
+            print(line)
+            print_stack(opcode['stack'], tabs)
+            print_opcodes(opcode['ops'], tabs + '\t')
+        else:
+            line = "\n{0}Step={1:0>8x} {2:<16}\tpc={3:0>8x} depth={4}".format(tabs, opcode['step'], opcode['op'],
+                                                                              opcode['pc'],
+                                                                              opcode['depth'])
+            print(line)
+            print_stack(opcode['stack'], tabs)
+
+
+def extract_access_list(hash, resp):
+    if "result" in resp:
+        record = {
+            'type': resp['result']['type'],
+            'h': hash,
+            'tt': resp['result']['tt'],
+            'ta': len(resp['result']['acl'])
+        }
+
+        a = []
+        s = []
+        ts = 0
+        for address, slots in resp['result']['acl'].items():
+            a.append(address)
+            if len(slots) > 0:
+                slots2 = {'address': address, 'storageKeys': list(slots.keys())}
+                ts = ts + len(slots2['storageKeys'])
+                s.append(slots2)
+
+        rd = {
+            'a': a
+        }
+
+        if len(s) > 0:
+            rd['s'] = s
+
+        record['ts'] = ts
+        record['rd'] = [rd]
+        return record
+    else:
+        return {'h': hash}
+
+
+def extract_block_access_list(block, resp):
+    results = []
+    if 'transactions' in block and 'result' in resp:
+        txs = block['transactions']
+        for idx, item in enumerate(resp['result']):
+            results.append(extract_access_list(txs[idx].hex(), item))
+    return results
+
+
+parser = argparse.ArgumentParser(description='Trace helper')
+parser.add_argument('--rpc', '-r', help='RPC server url, required', required=True)
+parser.add_argument('--tx', help='transaction hash or block number, required', required=True)
+parser.add_argument('--tracer', '-t', help='tracer name, default is ac_tracer', default='ac_tracer')
+parser.add_argument('--timeout', '-T', help='tracer timeout, in seconds', default=5)
+parser.add_argument('--out', '-o',
+                    help='tracer output file name prefix, tx and .json will be appended as the full name')
 
 if __name__ == '__main__':
 
-    if len(sys.argv) < 3:
-        print_help()
-        sys.exit(-1)
+    args = parser.parse_args()
 
-    url = sys.argv[1]
-    tx = sys.argv[2]
+    tracer_name = args.tracer
+    tracer = load_tracer(tracer_name)
+    http_provider = HTTPProvider(args.rpc)
 
-    if len(sys.argv) > 3:
-        tracer = load_tracer(sys.argv[3])
+    block = None
+    tx = args.tx
+    start_time = datetime.datetime.now()
+    if tx.isnumeric():
+        # Need to convert it to hex string
+        block_num = hex(int(tx))
+        block = fetch_block(http_provider, block_num)
+        resp = trace_block(http_provider, block_num, tracer)
     else:
-        tracer = None
+        resp = trace_tx(http_provider, tx, tracer)
+    end_time = datetime.datetime.now()
 
-    http_provider = HTTPProvider(url)
-    resp = trace_tx(http_provider, tx, tracer)
+    if tracer_name == 'ac_tracer':
+        if block is not None:
+            result = extract_block_access_list(block, resp)
+        else:
+            result = extract_access_list(tx, resp)
+    elif "result" in resp:
+        result = resp['result']
+    else:
+        result = resp
 
-    print(json.dumps(resp, indent=4))
+    if args.out is None:
+        if tracer_name == 'dis_tracer' and block is None:
+            print_opcodes(result)
+        else:
+            print(json.dumps(result, indent=4))
+    else:
+        filename = args.out + "-" + tx + ".json"
+        with open(filename, 'w', encoding='utf-8') as f:
+            json.dump(result, f, indent=4)
+
+    delta = end_time - start_time
+    print("\nTrace {0} time {1}s".format(tx, delta.total_seconds()))
--- trace/tracer/ac_tracer.js
+++ trace/tracer/ac_tracer.js
@@ -1,105 +1,67 @@
 var temp = {
+    type: 0,
     steps: 0,
-    stack: [{ops: []}],
-
-    topMemory: function (log, start, stop) {
-        return log.memory.slice(start, stop);
-    },
-    topStacks: function (log, n) {
-        var stacks = [];
-        n = n > log.stack.length() ? log.stack.length() : n;
-        for (var i = 0; i < n; i++) {
-            stacks.push(log.stack.peek(i).toString(16))
+    acList: {},
+    touches: 0,
+    addAddress: function (address) {
+        if (address in this.acList) {
+            return this.acList[address];
         }
-        return stacks;
+        var slots = {};
+        this.acList[address] = slots
+        return slots;
     },
-
-    latestOpcodes: function (frame, count) {
-        var start = frame["ops"].length - count;
-        if (start < 0) {
-            start = 0;
+    addSlot: function (address, key) {
+        var slots = this.addAddress(address)
+        if (!(key in slots)) {
+            slots[key] = ""
+            this.acList[address] = slots
+        }
+    },
+    result: function (ctx, db) {
+        from = toHex(ctx['from'])
+        this.addAddress(from)
+        if (ctx['to']) {
+            this.addAddress(toHex(ctx['to']))
+        } else {
+            // create contract
+            this.type = 2
         }
 
-        ops = []
-        frame["ops"].slice(start, frame["ops"].length)
-            .map(function (a) {
-                ops.push(a["op"]);
-            });
-        return ops;
-    },
-    acList: [],
-
-    addAddress: function (log, frame, address, op) {
-        var slot = {
-            contract: toHex(log.contract.getAddress()),
-            op: op,
-            address: address,
-            depth: log.getDepth(),
-            step: this.steps,
-            preops: this.latestOpcodes(frame, 16),
-            stacks: this.topStacks(log, 16)
+        return {
+            type: this.type,
+            tt: this.touches,
+            acl: this.acList
         }
-        this.acList.push(slot);
-    },
-    addSlot: function (log, frame, address, op, key) {
-        var slot = {
-            contract: address,
-            op: op,
-            address: address,
-            key: key,
-            depth: log.getDepth(),
-            step: this.steps,
-            preops: this.latestOpcodes(frame, 16),
-            stacks: this.topStacks(log, 16)
-        }
-        this.acList.push(slot);
-    },
-    result: function () {
-        return this.acList;
     },
     fault: function (log, db) {
     },
     step: function (log, db) {
-        var frame = this.stack[this.stack.length - 1];
-        var error = log.getError();
-        if (error) {
-            frame["error"] = error;
-        } else if (log.getDepth() == this.stack.length) {
-            var opinfo = {
-                op: log.op.toString(),
-                depth: log.getDepth()
-            };
-            if (frame.ops.length > 0) {
-                var prevop = frame.ops[frame.ops.length - 1];
-                if (prevop["op"].startsWith("PUSH")) {
-                    prevop["op"] = prevop["op"] + " " + log.stack.peek(0).toString(16)
-                }
-            }
-            switch (log.op.toString()) {
-                case "CALL":
-                case "CALLCODE":
-                case "DELEGATECALL":
-                case "STATICCALL":
-                    opinfo["to"] = log.stack.peek(1).toString(16);
-                    opinfo["ops"] = [];
-                    this.stack.push(opinfo);
-                    this.addAddress(log, frame, opinfo["to"], opinfo["op"])
-                    break;
-                case "EXTCODECOPY":
-                case "EXTCODESIZE":
-                case "EXTCODEHASH":
-                case "BALANCE":
-                    this.addAddress(log, frame, log.stack.peek(0).toString(16), opinfo["op"])
-                    break;
-                case "SLOAD":
-                case "SSTORE":
-                    opinfo["key"] = log.stack.peek(0).toString(16);
-                    this.addSlot(log, frame, toHex(log.contract.getAddress()), opinfo["op"], opinfo["key"])
-                    break;
-            }
-            frame.ops.push(opinfo);
-        } else {
-            this.stack = this.stack.slice(0, log.getDepth());
+        this.type = 1
+        if (log.getError()) {
+            return
+        }
+        switch (log.op.toString()) {
+            case "CALL":
+            case "CALLCODE":
+            case "DELEGATECALL":
+            case "STATICCALL":
+                this.addAddress(log.stack.peek(1).toString(16))
+                this.touches = this.touches + 1;
+                break;
+            case "EXTCODECOPY":
+            case "EXTCODESIZE":
+            case "EXTCODEHASH":
+            case "BALANCE":
+            case "SELFDESTRUCT":
+                this.addAddress(log.stack.peek(0).toString(16))
+                this.touches = this.touches + 1;
+                break;
+            case "SLOAD":
+                // case "SSTORE":
+                this.addSlot(toHex(log.contract.getAddress()), log.stack.peek(1).toString(16))
+                this.touches = this.touches + 1;
+                break;
         }
         this.steps++;
     }
